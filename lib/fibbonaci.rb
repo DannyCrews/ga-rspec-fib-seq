9e254f70094e962b13217bdff805537aac0d03dd
Dan Crews <dancrews@me.com>
2013-10-03 12:52:55 -0400

    set up rspec file structure. Wrote fibbonaci.rb.

---
+++ lib/fibbonaci.rb
@@ -0,0 +1,19 @@
+
+# produces a fibbonaci sequence in an array
+def fibbonaci_seq(num)
+   array = [0]
+
+  num.times do |i|
+    if i==0
+      array[i] = 1
+    elsif i==1
+      array[i] = 1
+    else
+      array[i] = array[i-1] + array[i-2]
+    end  
+  end
+
+  return array
+end
+
+puts fibbonaci_seq(5)
